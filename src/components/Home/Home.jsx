cc8c3868410237d2551cd89e2844739832296b53
Josue <josretamozovar@gmail.com>
2020-09-08 23:11:13 -0500

    added components  Home and Product , style image below header

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import shopping from "./../../media/images/market.jpg";
 import "./Home.scss";
 
-
 const Home = () => {
-  return <div></div>;
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img className="home__image" src={shopping} alt="shopping_market" />
+        <div className="home__row"></div>
+        <div className="home__row"></div>
+        <div className="home__row"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,10 +1,10 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyDLxMMI3VMu001JJG3zOCM7tCynjuCkxG4",
-  authDomain: "store-f2b11.firebaseapp.com",
-  databaseURL: "https://store-f2b11.firebaseio.com",
-  projectId: "store-f2b11",
-  storageBucket: "store-f2b11.appspot.com",
-  messagingSenderId: "523846097916",
-  appId: "1:523846097916:web:ff590287866d9804cab079",
-  measurementId: "G-ZLF2PN937E",
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDLxMMI3VMu001JJG3zOCM7tCynjuCkxG4",
+//   authDomain: "store-f2b11.firebaseapp.com",
+//   databaseURL: "https://store-f2b11.firebaseio.com",
+//   projectId: "store-f2b11",
+//   storageBucket: "store-f2b11.appspot.com",
+//   messagingSenderId: "523846097916",
+//   appId: "1:523846097916:web:ff590287866d9804cab079",
+//   measurementId: "G-ZLF2PN937E",
+// };
