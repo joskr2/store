65b9b79047ca19569b86c28c9c5fc8edb48b8b15
Josue <josretamozovar@gmail.com>
2020-09-08 20:00:05 -0500

    minor fix

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,18 +1,10 @@
-import * as firebase from "firebase";
-
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDLxMMI3VMu001JJG3zOCM7tCynjuCkxG4",
+  authDomain: "store-f2b11.firebaseapp.com",
+  databaseURL: "https://store-f2b11.firebaseio.com",
+  projectId: "store-f2b11",
+  storageBucket: "store-f2b11.appspot.com",
+  messagingSenderId: "523846097916",
+  appId: "1:523846097916:web:ff590287866d9804cab079",
+  measurementId: "G-ZLF2PN937E",
 };
-
-firebase.initializeApp(firebaseConfig);
-
-const database = firebase.database();
-
-export default database;
