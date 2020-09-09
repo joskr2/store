c09b2d882785c9567479e3fa5494c1bf7545f898
Josue <josretamozovar@gmail.com>
2020-09-09 10:33:55 -0500

    design product component

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
-import "./Product.scss"
+import React from "react";
+import "./Product.scss";
 
-const Product = () => {
-    return (
-        <div className="product">
-            
+const Product = (props) => {
+  const { titulo, precio, imagen, rating } = props;
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>{titulo}</p>
+        <p className="product__price">
+          <small>S/. </small>
+          <strong>{precio}</strong>
+        </p>
+        <div className="product__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <span index={i} role="img" aria-label="star">
+                ⭐
+              </span>
+            ))}
         </div>
-    )
-}
+      </div>
+      <img className="product__image" src={imagen} alt="producto" />
+      <button className="product__button">Agregar al carrito</button>
+    </div>
+  );
+};
 
-export default Product
+export default Product;
